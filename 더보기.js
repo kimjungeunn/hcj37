3b0ea8aeecf66ad28d612899add091ec29b17d25
unknown <ke050408@naver.com>
2026-04-12 21:07:08 +0900

    replace file4

--- 더보기.js
+++ 더보기.js
@@ -1,6 +1,9 @@
 document.querySelectorAll('.toggle').forEach((btn) => {
+  // label 태그는 JS 토글에서 제외
+  if (btn.tagName === 'LABEL') return
+
   btn.addEventListener('click', function () {
-    const desc = this.previousElementSibling
+    const desc = this.nextElementSibling
     if (desc.style.display === 'none') {
       desc.style.display = 'block'
       this.textContent = '접기 ▴'
